b91ed6c408deec0340e89ae9c13cafc581a460d2
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-14 22:24:53 +0800

    Added gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,18 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors: [
+              Colors.orange.shade900,
+              Colors.orange.shade400,
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
