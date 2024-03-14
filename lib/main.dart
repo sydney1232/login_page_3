53323845999f6023727a82efa1f14215a9156136
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-14 22:47:21 +0800

    Added Login and Welcome back Headers

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,38 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             colors: [
               Colors.orange.shade900,
+              Colors.orange.shade800,
               Colors.orange.shade400,
             ],
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 80),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  Text(
+                    "Welcome Back",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
